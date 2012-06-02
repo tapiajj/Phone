c17a3f63b99fe0f217d577b2a4b8f7a3c4f2647d
tapiajj <tapiajj@gmail.com>
2012-06-02 15:03:28 -0400

    Nothing yet, second commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Phone::Application.config.secret_token = 'bc3f4f23a3e3d2d2f60ab2674d8ea33e4825b05dfdee4b8fbcca0e735e2277e274c4ddca1dd72a271e5bec970468d180088c9e80cb53ac212d2b96e8ecb08051'
