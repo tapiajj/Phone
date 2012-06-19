fbd7c8c05763e52b7c668d3d31500d9c74938707
tapiajj <tapiajj@gmail.com>
2012-06-18 23:37:11 -0400

    somthing working :D

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Phone::Application.config.secret_token = '627ceca10b2a91ed962bac8c6e32f82d65c83042ba007e793953386458ec115ca0046a404a7316830186afb0350eed40ff7719cbd60c705b259231728bb19613'
